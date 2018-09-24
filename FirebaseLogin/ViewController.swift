25212333f3ad5b4807c7801ca6d37ca385ecfc0d
Nolan Fuchs <nolan1235@gmail.com>
2018-09-24 03:41:34 -0700

    Initial Commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Nolan Fuchs on 9/24/18.
+//  Copyright © 2018 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
